1e139def8d7055032ff9531653171b540bf21547
udayinbiswas21 <53809800+udayinbiswas21@users.noreply.github.com>
2020-09-05 13:10:24 +0530

    initialise 2d array

--- Arrays/2d_array_initialization.cpp
+++ Arrays/2d_array_initialization.cpp
@@ -14,7 +14,7 @@ void printArray(int array[],int length){
 int main(){
     // Declare + Initialise a 2d array in stack
     // Visualise as 2d table but memory allocated will be in a linear manner
-    int A[3][4];
+    int A[3][4] = {{0,1,2,3},{4,5,6,7},{8,9,10,11}};
 
     // Create an array of pointers and each pointer pointing to array in heap
     int *B[3];
@@ -27,6 +27,6 @@ int main(){
     C[0] = new int*[4];
     C[1] = new int*[4];
     C[2] = new int*[4];
-    
+
     return 0;
 }
