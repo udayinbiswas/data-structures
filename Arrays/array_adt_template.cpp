25b73cbe678c8b6694fc249d0e8ee0fd15a5b217
udayinbiswas21 <53809800+udayinbiswas21@users.noreply.github.com>
2020-09-06 12:48:10 +0530

    adt template class

---
+++ Arrays/array_adt_template.cpp
@@ -0,0 +1,66 @@
+// Array ADT template class implementation
+// Author : Udayin Biswas, Date : 06/09/2020
+
+#include <iostream>
+using namespace std;
+
+template<class T> 
+class Array {
+    private:
+        int *A;
+        int size;
+        int length;
+    public:
+        // Constructors
+        Array(){
+            size = 10;
+            length = 0;
+            // Declaring array in heap so that it can be accessed by class accessing construtor
+            A = new T[size]; 
+        }
+        Array(int size){
+            this->size = size;
+            length = 0;
+            A = new T[this->size];
+        }
+        //Destructor
+        ~Array(){
+            delete []A;
+        }
+        void Display();
+        void Insert(int index,T x);
+        T Delete(int index);
+
+};
+
+template<class T>
+void Array<T>::Display(){
+    for (int i=0;i<length;i++){
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
+}
+
+template<class T>
+void Array<T>::Insert(int index, T x){
+    if (index>=0 && index<=length){
+        for (int i=length-1;i>index;i--){
+            A[i] = A[i-1];
+        }
+        A[index] = x;
+        length++; 
+    }
+}
+
+template<class T>
+T Array<T>::Delete(int index){
+    T x = 0;
+    if (index>=0 && index<=length){
+        x = A[index];
+        for (int i=index;i<length;i++){
+            A[i] = A[i+1];
+        }
+        length--;
+    }
+    return x;
+}
