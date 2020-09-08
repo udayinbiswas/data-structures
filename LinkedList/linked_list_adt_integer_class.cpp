12fe85b538004888c26ace4d98cd7cb67c98c90e
udayinbiswas21 <53809800+udayinbiswas21@users.noreply.github.com>
2020-09-08 17:15:09 +0530

    template class added

--- LinkedList/linked_list_adt_integer_class.cpp
+++ LinkedList/linked_list_adt_integer_class.cpp
@@ -3,27 +3,25 @@
 #include <iostream>
 using namespace std;
 
-template<class T> 
 class  Node {
     public:
-        T data;
+        int data;
         Node *next;
 };
 
-template<class T> 
 class LinkedList {
     private:
-        Node<T> *head;
+        Node *head;
     public:
         // Constructors
         LinkedList(){head=NULL;}
-        LinkedList(T array[],int length){
-            head = new Node<T>; // declaring head in heap, not stack
+        LinkedList(int array[],int length){
+            head = new Node; // declaring head in heap, not stack
             // if we did Node *head; and then head=b where b is in heap, head will still be in stack and returning pointer 
             // would be useless in this to main function
-            Node<T> *p = head;
+            Node *p = head;
             for (int i=0;i<length;i++){
-                p->next = new Node<T>;
+                p->next = new Node;
                 p = p->next;
                 p->data = array[i];
                 p->next= NULL; // PUT THIS OTHERWISE SEGMENTATION FAULT WHILE DISPLAYING LATER. Last node should point to NULL 
@@ -33,7 +31,7 @@ class LinkedList {
         }
         // Destructor
         ~LinkedList(){
-            Node<T> *p = head;
+            Node *p = head;
             while (p!=NULL){
                 delete p;
                 p = p->next;
@@ -41,7 +39,7 @@ class LinkedList {
         }
         // Display linked list
         void Display(){
-            Node<T> *p = head;
+            Node *p = head;
             while (p!=NULL){
                 cout<<p->data<<" ";
                 p = p->next;
@@ -51,7 +49,7 @@ class LinkedList {
         }
         // Length of linked list
         int Length(){
-            Node<T> *p = head;
+            Node *p = head;
             int count = 0;
             while (p!=NULL){
                 count++;
@@ -60,20 +58,20 @@ class LinkedList {
             return count;
         }
         // Insert in Linked list
-        void Insert(T val, int index){
-            Node<T> *n = new Node<T>;
+        void Insert(int val, int index){
+            Node *n = new Node();
             n->data = val;
             if (index==0){
                 n->next = head;
                 head = n;
                 return;
             } 
-            Node<T> *p = head;
+            Node* p = head;
             int count = 1;
             while (p!=NULL){
                 if (index==count){
                     // Insert
-                    Node<T> *q = p->next;
+                    Node* q = p->next;
                     p->next = n;
                     n->next = q;
                     p = p->next;
@@ -86,24 +84,24 @@ class LinkedList {
         }
 
         // Delete from linked list
-        T Delete(int index){ // 0 indexed
+        int Delete(int index){ // 0 indexed
             if (index<0 || index>=Length()){
                 return -1;
             }
             if (index==0){
                 int val = head->data;
-                Node<T> *p = head;
+                Node *p = head;
                 head = head->next;
                 delete p;
                 return val;
             }
-            Node<T> *p = head;
+            Node *p = head;
             int count = 1;
             while (p!=NULL){
                 if (index==count){
                     if (p->next!=NULL){
                         int val = p->next->data;
-                        Node<T> *q = p->next->next;
+                        Node *q = p->next->next;
                         delete p->next;
                         p->next = q;
                         return val;
@@ -118,8 +116,8 @@ class LinkedList {
 };
 
 int main(){
-    float x[5] = {1,3.5,5,7,10};
-    LinkedList<float> l = LinkedList<float>(x,5);
+    int x[5] = {1,3,5,7,10};
+    LinkedList l = LinkedList(x,5);
     l.Display();
     cout<<"Length:"<<l.Length()<<endl;
     l.Insert(9,4);
