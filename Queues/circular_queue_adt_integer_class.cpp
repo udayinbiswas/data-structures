0af916f0a2f5adfb89615896548436db964446bf
udayinbiswas21 <53809800+udayinbiswas21@users.noreply.github.com>
2020-09-12 12:55:16 +0530

    circular queue

---
+++ Queues/circular_queue_adt_integer_class.cpp
@@ -0,0 +1,95 @@
+// Array ADT integer class implementation
+// Author : Udayin Biswas, Date : 06/09/2020
+
+#include <iostream>
+using namespace std;
+
+class CircularQueue {
+    private:
+        int *A;
+        int front; // points to the first element in queue
+        int rear; // points to the last element in queue
+        int size;
+    public:
+        CircularQueue();
+        CircularQueue(int size);
+        ~CircularQueue();
+        bool isFull();
+        bool isEmpty();
+        void enqueue(int x);
+        int dequeue();
+        void display();
+};
+
+CircularQueue::CircularQueue(){
+    A = new int[100];
+    front = rear = -1;
+}
+
+CircularQueue::CircularQueue(int size){
+    A = new int[size];
+    front = rear = -1;
+}
+
+CircularQueue::~CircularQueue(){
+    delete []A;
+}
+
+bool CircularQueue::isFull(){
+    if (front%size==(rear+1)%size){
+        return true;
+    }
+    return false;
+}
+
+bool CircularQueue::isEmpty(){
+    // When front=rear=-1,queue is empty
+    if (front==-1){
+        return true;
+    }
+    return false;
+}
+
+void CircularQueue::enqueue(int x){
+    if (isFull()) cout<<"Queue is full,cannot enequeue element "<<x<<endl;
+    if (isEmpty()){ // i.e. no element in queue
+        front = (front+1)%size;
+        rear = (rear+1)%size;
+    } else {
+        rear = (rear+1)%size;
+    }
+    A[rear]=x;
+    return;
+}
+
+int CircularQueue::dequeue(){
+    if (isEmpty()) cout<<"Queue is empty,cannot dequeue element"<<endl;
+    int val = -1;
+    if (front==rear){
+        val = A[front];
+        front = rear = -1;
+    } else {
+        val = A[front];
+        front = (front+1)%size;
+    }
+    return val;
+}
+
+void CircularQueue::display(){
+    if (front!=-1){
+        for (int i=front;i<=rear;i++){
+            cout<<A[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    CircularQueue q(20);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.display();
+    q.enqueue(7);
+    q.dequeue();
+    q.display();
+}
