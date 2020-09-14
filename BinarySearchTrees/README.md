4b02a6bfe18db726bc4e16c4cd3b40c50266bf19
udayinbiswas21 <53809800+udayinbiswas21@users.noreply.github.com>
2020-09-14 14:07:15 +0530

    recursive insert/delete bst

--- BinarySearchTrees/README.md
+++ BinarySearchTrees/README.md
@@ -5,3 +5,5 @@
 1. Inorder traversal of BST gives all the nodes in sorted order.
 
 2. An inorder list can have Catalan number of trees, which will all be BSTs too in this case. 
+
+3. To delete a node in a BST, find the inorder predecessor or indorder successor and replace node by it. If inorder successor/predecessor is a leaf, we don't need to shift any other elements. If it is not a leaf node, then multiple modifications have to be made. Number of modifications depends on the height i.e. deletion will be O(log n) just like for insertion.
--- BinarySearchTrees/bst_class.cpp
+++ BinarySearchTrees/bst_class.cpp
@@ -17,7 +17,7 @@ class BinarySearchTree{
     public:
         BinarySearchTree(){root=NULL;}
 
-        void Insert(int key){
+        void IterativeInsert(int key){
             
             Node *n = new Node;
             n->data = key;
@@ -46,6 +46,65 @@ class BinarySearchTree{
             return;
         }
 
+        void RecursiveInsert(int key){RecursiveInsert(key,root);}
+
+        // Notice how recursive insert is done by pointing the left or right pointer to returning node from a call to child.
+        Node* RecursiveInsert(int key,Node *p){
+            if (root==NULL){
+                Node *n = new Node;
+                n->data = key;
+                n->left = n->right = NULL;
+                root = n;
+                return n;
+            }
+
+            if (p==NULL){
+                Node *n = new Node;
+                n->data = key;
+                n->left = n->right = NULL;
+                return n;
+            }
+        
+            if (p->data>key){
+                p->left = RecursiveInsert(key,p->left);
+            } else {
+                p->right = RecursiveInsert(key,p->right);
+            }
+            return p;
+        }
+
+        void RecursiveDelete(int key){RecursiveDelete(key,root);}
+
+        // Notice how recursive DELETE is done by pointing the left or right pointer to returning node from a call to child.
+        Node* RecursiveDelete(int key,Node *p){
+            if (p==NULL){
+                return NULL;
+            }
+            if (p->data==key){
+                if (p->left!=NULL){
+                    Node *pred = inorderPredecessor(p);
+                    p->data = pred->data;
+                    p->left = RecursiveDelete(pred->data,p->left);
+                } else if (p->right!=NULL){
+                    Node *succ = inorderSuccessor(p);
+                    p->data = succ->data;
+                    p->right = RecursiveDelete(succ->data,p->right);
+                } else {
+                    // it is a leaf
+                    if (p==root){
+                        root = NULL;
+                    }
+                    delete p;
+                    return NULL;
+                }
+            } else if (p->data>key){
+                p->left = RecursiveDelete(key,p->left);
+            } else {
+                p->right = RecursiveDelete(key,p->right);
+            }
+            return p;
+        }
+
         void inOrderTraversal(){
             inOrderTraversal(root);
         }
@@ -72,6 +131,29 @@ class BinarySearchTree{
             return NULL;
         }
 
+        Node* inorderPredecessor(Node *p){
+            if (p->left!=NULL){
+                p = p->left;
+                while (p->right!=NULL){
+                    p = p->right;
+                }
+                return p;
+            }
+            return NULL;
+        }
+
+        Node* inorderSuccessor(Node *p){
+            if (p->right!=NULL){
+                p = p->right;
+                while (p->left!=NULL){
+                    p = p->left;
+                }
+                return p;
+            }
+            return NULL;
+        }
+
+
 };
 
 int main(){
@@ -79,18 +161,25 @@ int main(){
     BinarySearchTree bst;
  
     // Insert
-    bst.Insert(10);
-    bst.Insert(5);
-    bst.Insert(20);
-    bst.Insert(8);
-    bst.Insert(30);
- 
+    bst.RecursiveInsert(10);
+    bst.inOrderTraversal(); cout<<endl;
+    bst.RecursiveInsert(5);
+    bst.inOrderTraversal(); cout<<endl;
+    bst.RecursiveInsert(20);
+    bst.inOrderTraversal(); cout<<endl;
+    bst.RecursiveInsert(8);
+    bst.inOrderTraversal(); cout<<endl;
+    bst.RecursiveInsert(30);
+    bst.inOrderTraversal(); cout<<endl;
+    bst.RecursiveDelete(20);
+    bst.inOrderTraversal(); cout<<endl;
     // Inorder traversal
-    bst.inOrderTraversal();
-    cout << endl;
+    bst.RecursiveDelete(10);
+    bst.inOrderTraversal(); cout << endl;
+    
  
     // Search
-    Node* temp = bst.Search(2);
+    Node* temp = bst.Search(8);
     if (temp != NULL){
         cout << temp->data << endl;
     } else {
