4e90a36a23d16d1330a3496738e9497fee594673
udayinbiswas21 <53809800+udayinbiswas21@users.noreply.github.com>
2020-09-17 15:40:52 +0530

    heap delete operation

--- Heaps/README.md
+++ Heaps/README.md
@@ -10,3 +10,10 @@
 
 5. To insert n elements in a heap, it would take O(n log n) time.
 
+6. From heap, we can only delete root element which is highest priority element (max/min). Then take the lastmost element in heap according to array,delete it from original position and place it in root. Why lastmost element? So that tree remains complete and there are no blank spaces in array representation.
+
+7. Heap sort is basically deleting the max element at root from max heap and this element will go to the last position of the array. This we keep doing by deleting n elements until we find all elements sorted in ascending order eventually. Total time taken by heap sort is sum of creating the heap of O(n log n) and deleting elements 1 by 1 to make the array sorted of O(n log n). So O(2n log n)~O(n log n)
+
+Post this we compare root with its children and replace it with the bigger child each time, repeat this procedure.
+
+8. Priority queue is an abstract data structure which can be implemented by heap. Insert and deleting an element will both take O(log n) time.
--- Heaps/create_max_heap.cpp
+++ Heaps/create_max_heap.cpp
@@ -26,6 +26,27 @@ void PrintHeap(vector<int> &v){
     cout<<endl;
 }
 
+int DeleteFromHeap(vector<int> &v,int n){
+    int val = v[0];
+    v[0] = v[n];
+    v[n] = val;
+    int i = 0;
+    int j = 1;
+    while (j<n-1){
+        if (v[j]<v[j+1]) j=j+1;
+        if (v[i]<v[j]){
+            int t = v[i];
+            v[i] = v[j];
+            v[j] = t;
+            i = j;
+            j = 2*j;
+        } else {
+            break;
+        }
+    }
+    return val;
+}
+
 int main(){
     cout<<"Create Heap:"<<endl;
     vector<int> v;
@@ -38,6 +59,16 @@ int main(){
     v.push_back(35);
     PrintHeap(v);
     CreateHeap(v);
+    PrintHeap(v);
+    for (int i=v.size()-1;i>=0;i--){
+        // i will denote the last element of the heap. After every delete, we assume the array size reduces by 1
+        int x = DeleteFromHeap(v,i);
+        cout<<"Deleting element:"<<x<<endl;
+        cout<<"Array:";
+        PrintHeap(v);
+    }
+    // Note the final sorted order -> this is heap sort i.e. instead of deleting we are just shifting the max elements to
+    // the end of the array
     PrintHeap(v);
     return 0;
 }
